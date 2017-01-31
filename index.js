2d5f3bc2d545804aaffa3f035461c46e5f77b226
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-01-31 13:56:25 +0700

    basic craw to read out 'khuyen mai' news

--- index.js
+++ index.js
@@ -1,33 +1,86 @@
-var unirest = require("unirest");
+let  unirest = require("unirest");
+let cheerio = require('cheerio');
+let moment = require('moment');
+const timezone = 7 * 60; //vietnam
 
-var req = unirest("GET", "http://dichvumobifone.com/khuyen-mai");
+let sentMsg = [];
+
+let  req = unirest("GET", "http://dichvumobifone.com/khuyen-mai");
 req.headers({});
 req.end(function(res) {
     if (res.error) throw new Error(res.error);
 
-    // console.log(res.body);
-    let cheerio = require('cheerio')
     let $ = cheerio.load(res.body);
-    // let header = $('#header');
-    // console.log(header.text());
     let newsTitle = $('body > div.wrap-content > div > div > div.col-md-9.content')
     				.find('h3 > a')
-    				// .each(function(){
-    				// 	console.log($(this).text());
-    				// })
-    				// .map(function(){
-    				// 	console.log($(this).text());
-    				// 	return 1;
-    				// });
-    				// .map(() => $(this).text())
     				.map(function(){
-    					return $(this).text();
-    					// return 1;
+    					return $(this).text().toLowerCase();
     				})
-    				// .map(() => $(this).text())
-    				// .map(title => title.toLowerCase())
+    				.toArray();
     				;
-	// console.log(newsTitle);
-	// console.log(newsTitle[0].toLowerCase());
-	newsTitle = newsTitle.forEach(a => {console.log(a);});
+	let khuyenmaiNews = newsTitle.filter(title => {
+		let matches = title.match(/(khuyến mãi|thẻ|\d+\/\d+\/\d+)/g);
+
+		if(matches && matches[0] == 'khuyến mãi' && matches[1] == 'thẻ'){
+			let date = matches[2];
+			date = moment(date, 'D/M/YYYY').utcOffset(timezone);
+
+			if(!date.isValid())
+				return false;
+
+			let today = moment().utcOffset(timezone);
+			if(date.isAfter(today))
+				return true;
+
+			return false;
+		}
+
+		return false;
+	});
+
+	khuyenmaiNews = khuyenmaiNews.filter(news => {
+		if(sentMsg.includes(news))
+			return false;
+
+		return true;
+	});
+
+	if(khuyenmaiNews.length <= 0){
+		console.log('no news');
+		return false;
+	}
+
+
+	let slackMsgFields = khuyenmaiNews.map(title => {
+		let value = title;
+		let short = false;
+
+		return {value, short};
+	});
+
+	let slackMsg = {
+			text: `--- >>> ---`,
+			attachments: [
+				{
+					// title: '--- >< ---',
+					// title_link: 'https://tinker.press',
+					fields: slackMsgFields,
+					color: '#3381C5',
+					footer: '“The best way to predict the future is to create it.” – Peter Drucker  ᕕ( ᐛ )ᕗ',
+					footer_icon: 'https://tinker.press/favicon-64x64.png',
+					ts: Math.floor(new Date().getTime() / 1000)
+				}
+			]
+		};
+
+	var req = unirest("POST", "https://hooks.slack.com/services/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");
+	req.headers({});
+
+  	req.send(JSON.stringify(slackMsg));
+
+	req.end(function (res) {
+	  if (res.error) throw new Error(res.error);
+
+	  console.log(res.body);
+	});
 });
